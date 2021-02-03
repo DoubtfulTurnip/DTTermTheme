927f6fee2544b93bcdea1a499d0c7c9cddea434c
BuksheeTester <23531986+BuksheeTester@users.noreply.github.com>
2021-02-03 21:20:26 +1300

    Updates to theme installer to stop it from trying to re-install when not required

--- ThemeInstall.ps1
+++ ThemeInstall.ps1
@@ -1,35 +1,62 @@
 Write-Output "Installing BuksheePSTheme..."
-sleep 5
+Start-Sleep 5
 Write-Output "."
 Write-Output "."
 Write-Output "."
 Write-Output "Installing Modules"
 
-Install-Module posh-git -Scope CurrentUser -Force
-Install-Module oh-my-posh -Scope CurrentUser -Force
-Install-Module windows-screenfetch -Scope CurrentUser -Force
+if (Get-Module -ListAvailable -Name posh-git) {
+Write-Output "oh-my-posh Already Installed"
+} 
+else {
+            
+      Install-Module -Name oh-my-posh -Scope CurrentUser -AllowClobber -Confirm:$False -Force  
+     }
+
 Write-Output "."
 Write-Output "."
 Write-Output "."
 Write-Output "Copying Files..."
-New-Item -Path  C:\Programdata\BuksheePSTheme -ItemType Directory -Force  | Out-Null
-Copy-Item ./* C:\Programdata\BuksheePSTheme -r -force
-Copy-Item -Path "C:\ProgramData\BuksheePSTheme\Terminal Profile\settings.json" -Destination "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
-Copy-Item -Path "C:\ProgramData\BuksheePSTheme\PSProfile\Microsoft.PowerShell_profile.ps1" -destination "$env:UserProfile\Documents\WindowsPowerShell\" -Force
-Copy-Item -Path "C:\ProgramData\BuksheePSTheme\PSProfile\Microsoft.PowerShell_profile-7.ps1" -destination "$env:UserProfile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Force
-Copy-Item -Path "C:\ProgramData\BuksheePSTheme\PoshTheme\Paradox.psm1" -Destination "$env:UserProfile\Documents\WindowsPowerShell\Modules\oh-my-posh\2.0.496\Themes\Paradox.psm1" -Force
+$mydocuments = [environment]::getfolderpath("mydocuments")
+New-Item -Path  $mydocuments\BuksheePSTheme -ItemType Directory -Force  | Out-Null
+Copy-Item -Path ./* -Destination $mydocuments\BuksheePSTheme -r -force
+Copy-Item -Path "$mydocuments\BuksheePSTheme\Terminal Profile\settings.json" -Destination "$env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
+Copy-Item -Path "$mydocuments\BuksheePSTheme\PSProfile\Microsoft.PowerShell_profile.ps1" -destination "$env:UserProfile\Documents\WindowsPowerShell\" -Force
+Copy-Item -Path "$mydocuments\BuksheePSTheme\PSProfile\Microsoft.PowerShell_profile-7.ps1" -destination "$env:UserProfile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Force
+Copy-Item -Path "$mydocuments\BuksheePSTheme\PoshTheme\Paradox.psm1" -Destination "$env:UserProfile\Documents\WindowsPowerShell\Modules\oh-my-posh\2.0.496\Themes\Paradox.psm1" -Force
 Write-Output "."
 Write-Output "."
 Write-Output "."
-Write-Output "Installing Fonts - If the fonts already exist you will be asked if you want to overwrite - Either option is fine"
-$FONTS = 0x14
-$Path="c:\programdata\BuksheePSTheme\Fonts\"
-$objShell = New-Object -ComObject Shell.Application
-$objFolder = $objShell.Namespace($FONTS)
-$Fontdir = dir $Path
-foreach($File in $Fontdir) {
-  $objFolder.CopyHere($File.fullname, 16)
-}
+Write-Output "Installing Fonts"
+Write-Output "."
+Write-Output "."
+Write-Output "."
+$checkfont1 = Test-Path -Path "$env:LOCALAPPDATA\Microsoft\Windows\Fonts\CascadiaCodePL.ttf"
+if ($checkfont1 -eq "true") {
+  Write-Output "CascadiaCodePL Font Already Installed"
+  } 
+  else {        
+    $FONTS = 0x14
+    $objShell = New-Object -ComObject Shell.Application
+    $objFolder = $objShell.Namespace($FONTS)
+    $objFolder.CopyHere("$mydocuments\BuksheePSTheme\Fonts\CascadiaCodePL.ttf")  
+       }
+Write-Output "."
+Write-Output "."
+Write-Output "."
+$checkfont2 = Test-Path -Path "$env:LOCALAPPDATA\Microsoft\Windows\Fonts\Ubuntu-R.ttf"
+if ($checkfont2 -eq "true") {
+   Write-Output "Ubuntu-R Font Already Installed"
+   } 
+   else {        
+     $FONTS = 0x14
+     $objShell = New-Object -ComObject Shell.Application
+     $objFolder = $objShell.Namespace($FONTS)
+     $objFolder.CopyHere("$mydocuments\BuksheePSTheme\Fonts\CascadiaCodePL.ttf")  
+        }
+Write-Output "."
+Write-Output "."
+Write-Output "."
 
 #Installing updates for WSL Ubuntu 20 and Kali
 Write-Output "."
@@ -41,7 +68,7 @@ Write-Output "."
 Write-Output "."
 Write-Output "."
 Write-Output "Installing Ubuntu Feature - Enter Sudo Password"
-wsl.exe -d Ubuntu-20.04 sh -c "sudo apt update && sudo apt install neofetch -y && echo neofetch >> ~/.profile"
+wsl.exe -d Ubuntu-20.04 sh -c "sudo apt update && sudo apt install neofetch -y && echo neofetch >> ~/.profile && touch ~/.hushlogin"
 Write-Output "."
 Write-Output "."
 Write-Output "."
@@ -59,5 +86,5 @@ Write-Output "."
 
 #Final Message
 Write-Output "Theme installation is now complete - Restart your PowerShell instance"
-Sleep -Seconds 10
-
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
